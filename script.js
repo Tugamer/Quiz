7cfe51d12718b2878b629fe63aa82f03c55ff69c
Tugamer <tu.gamer321@gmail.com>
2025-02-03 06:50:52 -0300

    consertando carregamento

--- script.js
+++ script.js
@@ -270,15 +270,14 @@ function reiniciarQuiz() {
 document.addEventListener('DOMContentLoaded', function() {
   console.log("DOM carregado, iniciando pré-carregamento...");
 
-   // Teste com um arquivo de cada vez
-   const arquivosParaCarregar = [
-    'images/princesa.gif',
-    'audios/Lush life.mp3'
-    'audios/som de interrupção.mp3'
-    'audios/hisoka music edited version.mp3'
-    'images/novo.gif'
-    
-  ];
+const arquivosParaCarregar = [
+  'images/princesa.gif',
+  'audios/Lush life.mp3',  
+  'audios/som de interrupção.mp3',  
+  'audios/hisoka music edited version.mp3',  
+  'images/novo.gif'
+];
+
   let carregados = 0;
   function verificarCarregamento() {
     carregados++;
